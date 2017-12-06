7f670d6243d39bc61fc44c1f01c898b57718c71b
Kevin Babcock <Kevin_Babcock@Kevins-MacBook-Pro.local>
2017-12-06 11:45:48 -0800

    add integration test for homepage

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,16 +1,16 @@
-# require("capybara/rspec")
-# require("./app")
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('adding a new word', {:type => :feature}) do
-#   it('creates a new page for the word submitted by user') do
-#     visit("/")
-#     fill_in("addWord", :with => "baseball")
-#     click_button("Submit")
-#     expect(page).to have_content("baseball")
-#   end
-# end
+require("capybara/rspec")
+require("./app")
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('adding a new word', {:type => :feature}) do
+  it('creates a new page for the word submitted by user') do
+    visit("/")
+    fill_in("addWord", :with => "baseball")
+    click_button("Submit")
+    expect(page).to have_content("baseball")
+  end
+end
 
 # describe('adding a new definition', {:type => :feature}) do
 #   it('adds definition to that words page') do
